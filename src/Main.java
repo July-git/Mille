b3c355ed3f762502bb0c45186b6e7c7c301595ae
July-git <80923518+July-git@users.noreply.github.com>
2021-04-09 17:58:57 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    long ticketPrice = 2530;
+        long ticketPrice = 2530;
         int mileBonus = 20;
         long totalBonus = ticketPrice / mileBonus;
         System.out.println(totalBonus);
